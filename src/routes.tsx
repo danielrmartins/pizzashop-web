8621d87ad91485bce15b6c06913cfa2b32b34af5
Daniel <danielmartins10@gmail.com>
2024-10-07 17:12:27 -0300

    chore: add `order-table-filters` and `order-table-row`

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,9 +3,9 @@ import { createBrowserRouter } from 'react-router-dom'
 import { AppLayout } from './pages/_layout/app'
 import { AuthLayout } from './pages/_layout/auth'
 import { Dashboard } from './pages/app/dashboard'
+import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
-import { Orders } from './pages/orders/orders'
 
 export const router = createBrowserRouter([
   {
